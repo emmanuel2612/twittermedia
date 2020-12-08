0a3db8e6cedd33e692cf625f8883d8459016a736
emmanuel2612 <64177390+emmanuel2612@users.noreply.github.com>
2020-12-08 13:29:18 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -8,15 +8,24 @@ window.onload = () =>{
 
     const wrap = document.querySelector('#wrap');
 
+
     gifsButton.onclick = () =>{
 
     wrap.style.transform = "translateX(-100%)";
+
+    gifsButton.style.color = "white";
+    photosButton.style.color = "#8997A2";
+
+
         
     }
 
     photosButton.onclick = () =>{
 
         wrap.style.transform = "translateX(0%)";
+
+        photosButton.style.color = "white";
+        gifsButton.style.color = "#8997A2";
     
 
     }
